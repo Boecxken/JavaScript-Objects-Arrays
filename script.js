2e54810d54713476dbfd5aeabe79a095e36247bc
Boecxken <84395269+Boecxken@users.noreply.github.com>
2021-06-05 16:12:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,23 +4,19 @@ let person = {
     evaluations: [7, 10, 9]
 };
 
-/* Dot notation
-person.name = "Susanne";
-person.age = 28;
-person.evaluations = 7, 10, 9;
-*/
-
-/* Bracket notation
-person["name"] = "Susanne";
-person["age"] = 28;
-*/
-
 console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person["name"]);
+console.log(person["age"]);
+console.log(person.evaluations);
+
 
 let selectedColors = ["groen", "blauw", "rood"];
 console.log(selectedColors);
 console.log(selectedColors.length);
 console.log(selectedColors[0]);
+console.log(selectedColors[selectedColors - 1]);
 
 selectedColors.push("geel");
 console.log(selectedColors);
@@ -28,7 +24,7 @@ console.log(selectedColors);
 selectedColors.push(5);
 console.log(selectedColors);
 
-selectedColors.push("{greeting: Hi ik ben een object}");
+selectedColors.push({ greeting: "Hi ik ben een object" });
 console.log(selectedColors);
 console.log(selectedColors[5]);
 
